018d769813f5ba01cfc7416872d5a38bcd77b368
fazelpejmanfar <fazelpejmanfar@gmail.com>
2023-09-15 22:28:40 +0400

    fix button sizes on mobile + fix toaster borders

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
-import '../styles/globals.css'
-import Web3Providers from '../providers/Connector'
-import { Toaster } from 'react-hot-toast'
-import { ToastOptions } from '../lib/toastOptions'
+import "../styles/globals.css";
+import Web3Providers from "../providers/Connector";
+import { Toaster } from "react-hot-toast";
+import { ToastOptions } from "../lib/toastOptions";
 function MyApp({ Component, pageProps }) {
   return (
     <Web3Providers>
-    <Toaster toastOptions={ToastOptions}/>
-  <Component {...pageProps} />
-  </Web3Providers>
-)}
+      <Toaster toastOptions={ToastOptions} />
+      <Component {...pageProps} />
+    </Web3Providers>
+  );
+}
 
-export default MyApp
+export default MyApp;
